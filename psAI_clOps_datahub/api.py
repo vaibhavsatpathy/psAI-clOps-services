7c46ee4f3349288a072d7a5ad3ab6b3312c19941
Vaibhav Satpathy <vasatpathy@deloitte.com>
2021-01-21 16:46:36 +0530

    Added CORS

--- psAI_clOps_datahub/api.py
+++ psAI_clOps_datahub/api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from psAI_clOps_datahub.model_data_route import model_data_router
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router=model_data_router, tags=["Model Description"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
